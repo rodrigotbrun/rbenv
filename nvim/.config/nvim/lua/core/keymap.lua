a8e42af1e4c1237e7f8e2930678eb93673afb7d0
Rodrigo Brun <rodrigotbrun@gmail.com>
2025-09-29 12:07:02 -0300

    search and replace current word

--- nvim/.config/nvim/lua/core/keymap.lua
+++ nvim/.config/nvim/lua/core/keymap.lua
@@ -14,7 +14,7 @@ vim.keymap.set("v", "<", "<gv", opts)
 vim.keymap.set("v", ">", ">gv", opts)
 
 -- delete without copy --
-vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+vim.keymap.set("x", "<leader>p", '"_dP')
 
 -- indent with LSP
 vim.keymap.set("n", "<leader>L", vim.lsp.buf.format)
@@ -22,6 +22,7 @@ vim.keymap.set("n", "<leader>L", vim.lsp.buf.format)
 vim.keymap.set("n", "Q", "<nop>")
 
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true, desc = "makes file executable" })
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 -- highlight yank/copy
 vim.api.nvim_create_autocmd("TextYankPost", {
