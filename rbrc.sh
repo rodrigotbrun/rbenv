3754dec19a79b2d3169559a4fcb4d5e93845d7d0
Rodrigo Brun <rodrigotbrun@gmail.com>
2025-10-02 13:59:07 -0300

    added TERMINAL_EMULATOR flag, to avoid auto attach session to other terminals
    Set TERMINAL_EMULATOR to anything on phpstorm for example, so it won't
    start tmux on the built in terminal

--- rbrc.sh
+++ rbrc.sh
@@ -15,7 +15,7 @@ source $HOME/.rbdev/rbaliases.sh
 source $HOME/.rbdev/rbfunctions.sh
 
 # Restart to my tmux session
-if [[ -z $TMUX ]]; then
+if [[ -z $TMUX  && "$TERMINAL_EMULATOR" == "" ]]; then
     session_name="dev" # You can customize the session name
     if tmux has-session -t "$session_name" 2>/dev/null; then
         exec tmux attach-session -t "$session_name"
