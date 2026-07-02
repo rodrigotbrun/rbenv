8948d0530b79cb35408331d3cff958872f185205
Rodrigo Brun <rodrigotbrun@gmail.com>
2026-07-02 18:11:19 -0300

    new stuff

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,22 +1,56 @@
--- return {
--- 	"rebelot/kanagawa.nvim",
--- 	lazy = false,
--- 	priority = 1000,
--- 	config = function()
--- 		vim.cmd.colorscheme("kanagawa")
--- 	end,
--- }
+local function patch_popup_highlights()
+	local normal = vim.api.nvim_get_hl(0, { name = "Normal", link = false })
+	local visual = vim.api.nvim_get_hl(0, { name = "Visual", link = false })
+
+	local fg = normal.fg
+	local bg = normal.bg
+	local sel_bg = visual.bg or normal.bg
+
+	local function hl(name, opts)
+		vim.api.nvim_set_hl(0, name, vim.tbl_extend("force", { fg = fg, bg = bg }, opts))
+	end
+
+	hl("NormalFloat", {})
+	hl("Pmenu", {})
+	hl("FloatBorder", { fg = bg })
+	hl("FloatTitle", { bold = true })
+	hl("FloatFooter", { bold = true })
+	hl("PmenuSel", { bg = sel_bg })
+	hl("PmenuSbar", { bg = bg })
+	hl("PmenuThumb", { bg = sel_bg })
+
+	local telescope_groups = {
+		"TelescopeNormal",
+		"TelescopeBorder",
+		"TelescopePromptNormal",
+		"TelescopePromptBorder",
+		"TelescopeResultsNormal",
+		"TelescopeResultsBorder",
+		"TelescopePreviewNormal",
+		"TelescopePreviewBorder",
+		"TelescopeSelection",
+		"TelescopeSelectionCaret",
+	}
+
+	for _, group in ipairs(telescope_groups) do
+		vim.api.nvim_set_hl(0, group, { link = "NormalFloat" })
+	end
+end
 
 return {
-	"projekt0n/github-nvim-theme",
-	name = "github-theme",
-	lazy = false, -- make sure we load this during startup if it is your main colorscheme
-	priority = 1000, -- make sure to load this before all the other start plugins
+	"CosecSecCot/midnight-desert.nvim",
+	lazy = false,
+	priority = 1000,
+	dependencies = {
+		"rktjmp/lush.nvim",
+	},
 	config = function()
-		require("github-theme").setup({
-			-- ...
-		})
+		vim.cmd.colorscheme("midnight-desert")
+		patch_popup_highlights()
 
-		vim.cmd("colorscheme github_dark_default")
+		vim.api.nvim_create_autocmd("ColorScheme", {
+			pattern = "midnight-desert",
+			callback = patch_popup_highlights,
+		})
 	end,
 }
