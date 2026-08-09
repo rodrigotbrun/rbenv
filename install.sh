308b7974c4a7ee0cce126e45a75bfc0d82296e93
Rodrigo Brun <rodrigotbrun@gmail.com>
2026-08-09 18:20:50 -0300

    macos

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export RB_HOME=$(pwd);
+export RB_HOME=$(pwd)
 
 # Install xCode cli tools
 if [[ "$(uname)" == "Darwin" ]]; then
@@ -23,26 +23,33 @@ npm install -g ios-deploy
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 brew install coreutils zplug stow zsh-autosuggestions zsh-syntax-highlighting \
-  fzf bat fd zoxide lua luajit luarocks prettier make qmk lazygit \
-  lazykube lazydocker tree-sitter tree borders imagemagick htop btop
+    fzf bat fd zoxide lua luajit luarocks prettier make qmk lazygit \
+    lazykube lazydocker tree-sitter tree borders imagemagick htop btop
 
 brew install --cask font-hack-nerd-font
 brew install --cask font-jetbrains-mono-nerd-font
 brew install --cask font-sf-pro
+
+brew install --cask zulu@17
+brew info --cask zulu@17
 
 # Install softwares
 brew install --cask iterm2
 brew install --cask tableplus
 brew install --cask phpstorm
 brew install --cask 1password
+brew install --cask android-studio
+brew install --cask openlogi
+brew install --cask postman
+brew install --cask macfuse
+brew install borgbackup/tap/borgbackup-fuse
+brew install --cask vorta
 
 # mac settings
 sudo defaults write NSGlobalDomain KeyRepeat -int 2
 sudo defaults write InitialKeyRepeat -int 15
 csrutil status
 
-echo 'export PATH="/opt/homebrew/opt/coreutils/libexec/gnubin:$PATH"' >> ~/.zshrc
+echo 'export PATH="/opt/homebrew/opt/coreutils/libexec/gnubin:$PATH"' >>~/.zshrc
 
-echo 'source $RB_HOME/rbrc.sh' >> ~/.zshrc
-
-
+echo 'source $RB_HOME/rbrc.sh' >>~/.zshrc
